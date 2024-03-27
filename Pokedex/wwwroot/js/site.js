eea772ce9e32908f9ff1b0a009209b6921bdb722
anglusa <otavio.baroni@etec.sp.gov.br>
2024-03-27 14:15:45 -0300

    cabou

--- Pokedex/wwwroot/js/site.js
+++ Pokedex/wwwroot/js/site.js
@@ -1,8 +1,8 @@
-﻿function filter (type) {
-    let card, i;
+﻿function filter(type) {
+    let cards, i;
     let count = 0;
-    cards = document.getElementsByClassName("cards");
-    buttons = DocumentElementsByClassName("btn-filter");
+    cards = document.getElementsByClassName("card");
+    buttons = document.getElementsByClassName("btn-filter");
     for (i = 0; i < cards.length; i++) {
         cards[i].parentElement.style.display = 'none';
         if (cards[i].classList.contains(type) || type === 'all') {
@@ -10,4 +10,22 @@
             count += 1;
         };
     };
+    for (i = 0; i < buttons.length; i++) {
+        if (buttons[i].id == `btn-${ type }`) {
+            buttons[i].classList.remove("btn-sm");
+            buttons[i].classList.add("btn-md");
+        }
+    else {
+            buttons[i].classList.remove("btn-md");
+            buttons[i].classList.add("btn-sm");
+        }
+    };
+    if (type === "all"){
+        document.getElementById("btn-all").classList.remove("btn-sm");
+        document.getElementById("btn-all").classList.add("btn-md");
+    };
+    if(count ==0)
+    document.getElementById("zeroPokemon").classList.remove("d-none");
+    else
+    document.getElementById("zeroPokemon").classList.add("d-none");
 }
